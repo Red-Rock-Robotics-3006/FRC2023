255714983f796e2bb6549886e644af647f6c1c33
llessurfish <llesshiface@gmail.com>
2023-02-21 15:54:16 -0800

    Added code to setTargetPos method
    Also adds an instance variable called targetPos. Currently there is no safety in setting, may need to update later.

--- src/main/java/frc/robot/Subsystems/ArmActuator.java
+++ src/main/java/frc/robot/Subsystems/ArmActuator.java
@@ -4,7 +4,11 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmActuator extends SubsystemBase {
-    public void setTargetPos(Translation2d pos) {}
+    Translation2d targetPos;
+    public void setTargetPos(Translation2d pos) 
+    {
+        targetPos = pos;
+    }
     public void setTargetPosSafeTravel(Translation2d pos, double arcRadius) {}
     public Translation2d getMeasuredPos() { return new Translation2d(); }
 }
