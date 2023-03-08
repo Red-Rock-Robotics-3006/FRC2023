4aa8cab6cd2d54de01abbf2b94800373ae919960
llessurfish <llesshiface@gmail.com>
2023-03-08 15:27:13 -0800

    Motor control made
    Should be almost done, just need position and target inputs

--- src/main/java/frc/robot/Subsystems/ArmActuator.java
+++ src/main/java/frc/robot/Subsystems/ArmActuator.java
@@ -3,10 +3,15 @@ package frc.robot.Subsystems;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
+
 /*TODO
  * Get max values of rotation and position for x and y
- * Get the motor control function
- * Fix the dynamic motor control while loop
+ * Get info from encoders
+ * Make dynamic motor control
+ * Get target from (limelight?)
  * Probably something else I'm forgetting
  */
 
@@ -14,7 +19,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmActuator extends SubsystemBase {
 
+    public CANSparkMax yMotor = new CANSparkMax(61, CANSparkMaxLowLevel.MotorType.kBrushed);
+    public CANSparkMax xMotor = new CANSparkMax(62, CANSparkMaxLowLevel.MotorType.kBrushed);
 
+
+    public ArmActuator() {
+
+        this.yMotor.restoreFactoryDefaults();
+        this.yMotor.setInverted(false);
+        this.yMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
+        this.xMotor.restoreFactoryDefaults();
+        this.xMotor.setInverted(false);
+        this.xMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
+    }
+
+
+    private Translation2d target;
 
     /* Replace with min and max values of the absolute encoders on x & y axis */
     private final double yRotMin = 0.0;
@@ -48,27 +70,30 @@ public class ArmActuator extends SubsystemBase {
         targetY = targetPos.getY();
 
         
-        /* Keep arm updating and moving until it reaches targetPos */
-        while(xDiff!=0||yDiff!=0){
-            double xDiff = xPos - targetX;
-            double yDiff = yPos - targetY;
+        double xDiff = xPos - targetX;
+        double yDiff = yPos - targetY;
 
-            moveToPos(yDiff, xDiff);
-        }
+        moveToPos(yDiff, xDiff);
+        
     }
     public void setTargetPosSafeTravel(Translation2d pos, double arcRadius) {}
     public Translation2d getMeasuredPos() { return new Translation2d(); }
 
     /* Needs to be run repeatedly to make the arm slow to a stop */
-    public double moveToPos(double vDiff, double hDiff){
+    public void moveToPos(double vDiff, double hDiff){
         
         
         double yMotorPercent = (Math.cbrt(vDiff) / Math.cbrt(yMax - yMin));
         double xMotorPercent = (Math.cbrt(hDiff) / Math.cbrt(xMax - xMin));
 
         
-        moveYMotor(yMotorPercent);
-        moveXMotor(xMotorPercent);
+        this.yMotor.set(yMotorPercent);
+        this.xMotor.set(xMotorPercent);
         
     }
+
+    public void periodic(){
+        setTargetPos(target);
+    }
+
 }
