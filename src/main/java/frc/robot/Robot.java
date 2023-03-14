3dfcc0c94d76fd169fc6eb4f8e558dca93b3e9ec
llessurfish <llesshiface@gmail.com>
2023-03-14 15:15:58 -0700

    Now allows selecting autos from smartdashboard
    using SendableChooser

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,20 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Robot extends TimedRobot {
+
+
+
+  // Make options for autos and SendableChooser object
+  private static final String kFirstAuto = "First Auto";
+  private static final String kSecondAuto = "Second Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+
   private final RobotContainer m_robotContainer = new RobotContainer();
 
   /**
@@ -16,6 +29,13 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+
+
+    // Set autos as options for the sendablechooser
+    m_chooser.setDefaultOption("First Auto", kFirstAuto);
+    m_chooser.addOption("Second Auto", kSecondAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+
     // Flush NetworkTables every loop. This ensures that robot pose and other values
     // are sent during every loop iteration.
     setNetworkTablesFlushEnabled(true);
@@ -28,7 +48,26 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
+
+    m_autoSelected = m_chooser.getSelected();
+    System.out.println("Auto selected: " + m_autoSelected);
     m_robotContainer.getAutonomousCommand().schedule();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kSecondAuto:
+      // Probably call to auto method?
+      System.out.println("The first auto should run here");
+      break;
+      case kFirstAuto:
+      default:
+      // Auto code?
+      System.out.println("The first auto should run here");
+      break;
+
+    }
   }
 
   @Override
