395c8aa319c184067d299374840ef52b45fbfbca
ThomasBestvina <bestvinathomas@gmail.com>
2023-03-18 14:19:21 -0600

    Pneumatics support added.

--- src/main/java/frc/robot/Subsystems/EndEffector.java
+++ src/main/java/frc/robot/Subsystems/EndEffector.java
@@ -1,14 +1,29 @@
 package frc.robot.Subsystems;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+
 public class EndEffector extends SubsystemBase {
+
+    DoubleSolenoid handSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+
+    private boolean pcmMode = false; //True is forward, false is back
+
     public void setTargetAngle(double angle) {}
     public void intake(Mode mode) {}
     public void expel(Mode mode) {}
     public Translation2d getTargetCenterOffset() { return new Translation2d(); }
     public double getMeasuredAngle() { return 0; }
+
+    public void togglePneumatics()
+    {
+        pcmMode = !pcmMode;
+        if(pcmMode) handSolenoid.set(kForward);
+        else{ handSolenoid.set(kReverse); }
 
     public enum Mode {
         CUBE,
